ae211ecddb837f9bed8b4ddd4238f0dd6cddb651
vishnurdas27 <vishnurdas27@gmail.com>
2026-01-27 12:30:23 +0530

    commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,29 @@
 import { serve } from "inngest/next";
+import { Inngest } from "inngest";
+import connectDB from "@/config/db"; // Check this path!
+import User from "@/models/User";    // Check this path!
 
-// ⚠️ IMPORTANT: Note the path points to your 'server' folder
-import { inngest, functions } from "../../../server/inggest/index.js"; 
+// 1. Define Inngest Client directly here
+const inngest = new Inngest({ id: "cinespot" });
 
+// 2. Define Function directly here
+const syncUserCreation = inngest.createFunction(
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        await connectDB();
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+        await User.create({
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        });
+    }
+);
+
+// 3. Serve the API
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions,
+  functions: [syncUserCreation], // Add other functions here
 });

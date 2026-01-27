f56411b4b58b4af6a1dc9b2da9233ed80bc1df7a
vishnurdas27 <vishnurdas27@gmail.com>
2026-01-27 11:43:15 +0530

    Initial commit

--- server/inggest/index.js
+++ server/inggest/index.js
@@ -1,10 +1,10 @@
 import { Inngest } from "inngest";
-import User from "../models/user";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "cinespot" });
 
 // Function to save user data to database
-const syncUserCreation = inggest.createFunction(
+const syncUserCreation = Inggest.createFunction(
     {id: 'sync-user-from-clerk'},
     {event: 'clerk/user.created'},
     async ({event})=>{
@@ -19,5 +19,32 @@ const syncUserCreation = inggest.createFunction(
     }
 )
 
+//Function to delete user from database
+const syncUserDeletion = Inggest.createFunction(
+    {id: 'delete-user-from-clerk'},
+    {event: 'clerk/user.deleted'},
+    async ({event})=>{
+        const {id} = event.data 
+        await User.findByIdAndDelete(id)
+    }
+)
 
-export const functions = [syncUserCreation];
+//Function to update user from database
+const syncUserUpdation = Inggest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+    async ({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id :id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+export const functions = [syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation];
